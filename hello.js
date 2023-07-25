7294edff01e32d9ef8e58491b496ccd7a02dfe91
j-alam777 <mr.jahangir.alam@gmail.com>
2023-07-25 21:38:22 +0600

    js file added

---
+++ hello.js
@@ -0,0 +1,28 @@
+console.log ("hello world");
+var studentName = "alam";
+console.log (studentName);
+var num1 = 20;
+var num2 = 30;
+var total = num1 + num2;
+console.log (total);
+
+var num1 = 40;
+var num2 = 30;
+var result = num1 - num2;
+console.log (result);
+
+var num1 = 40;
+var num2 = 30;
+var result = num1 * num2;
+console.log (result);
+
+var num1 = 40;
+var num2 = 10;
+var result = num1 / num2;
+console.log (result);
+
+var num1 = 42;
+var num2 = 4;
+var result = num1 % num2;
+console.log (result);
+
